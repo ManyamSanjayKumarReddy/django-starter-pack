a40998814a8ca852c0f2903fe67a246915cd2edb
sanjaykumarreddymanyam <entrepreneur.sanju@gmail.com>
2025-12-05 16:47:32 +0530

    starter project initilization

---
+++ README.md
@@ -0,0 +1,7 @@
+# Django Starter Application
+
+1. uv init
+2. add django 5.2
+3. django-admin startproject config .
+4. python manage.py runserver 
+5. 
---
+++ api/api_router.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.views import LogoutView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+
+urlpatterns = [
+    # path('auth/register/', RegisterView.as_view(), name='register'),
+]
